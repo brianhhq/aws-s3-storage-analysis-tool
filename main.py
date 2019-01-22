113f5d11811dbadaa8f3ed27259380e7aad29c91
brian-hyperanna <brian@hyperanna.com>
2019-01-22 23:22:41 +1100

    [Multiprocessing] 10m34.271s vs 11m14.751s

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 import boto3
+import multiprocessing as mp
 
 
-def main():
-    results=[]
-    s3 = boto3.client('s3')
-    response = s3.list_buckets()
-
-    for bucket in response['Buckets']:
-        total_size = 0
-        num_of_files = 0
-        result = {
-            'Name': bucket['Name'],
-            'CreationDate': str(bucket['CreationDate'])
-        }
-        bucket = boto3.resource('s3').Bucket(result['Name'])
-        for item in bucket.objects.all():
-            num_of_files += 1
-            total_size += item.size
-        result['Number of Files'] = num_of_files
-        result['Total Size'] = total_size / 1000000000
-        results.append(result)
-        print(result)
-    print(results)
+def get_bucket_info(bucket):
+    total_size = 0
+    num_of_files = 0
+    result = {
+        'Name': bucket['Name'],
+        'CreationDate': str(bucket['CreationDate'])
+    }
+    b = boto3.resource('s3').Bucket(result['Name'])
+    for item in b.objects.all():
+        num_of_files += 1
+        total_size += item.size
+    result['Number of Files'] = num_of_files
+    result['Total Size'] = total_size / 1000000000
+    return result
 
 
 if __name__ == "__main__":
-    main()
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+    pool = mp.Pool(7)
+    results = []
+    for bucket_item in response['Buckets']:
+        r = pool.apply_async(get_bucket_info, [bucket_item])
+        results.append(r.get())
+    pool.close()
+    print(results)
